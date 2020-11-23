9a42a2cd8c199aa1db821b84e3f7e4aefe0cfe4e
Iñigo Bereciartua <beretxiki99@gmail.com>
2020-11-23 03:08:39 -0800

    Initial Commit

---
+++ Native Apps Music App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Native Apps Music App
+//
+//  Created by Iñigo Bereciartua on 23/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Native Apps Music AppTests/Native_Apps_Music_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Native_Apps_Music_AppTests.swift
+//  Native Apps Music AppTests
+//
+//  Created by Iñigo Bereciartua on 23/11/2020.
+//
+
+import XCTest
+@testable import Native_Apps_Music_App
+
+class Native_Apps_Music_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
